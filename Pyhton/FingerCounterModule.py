ab62f1f4e8ac154fd8f7977466dc7b61c959eaf9
KingOfTNT10 <66069146+KingOfTNT10@users.noreply.github.com>
2021-10-20 22:35:35 +0300

    Update FingerCounterModule.py

--- Pyhton/FingerCounterModule.py
+++ Pyhton/FingerCounterModule.py
@@ -6,8 +6,8 @@ class fingerCounter():
         self.detector = htm.handDetector(maxHands=1)
         self.tipIds = [4, 8, 12, 16, 20]
 
-    def getFingerCount(self, img, draw=True):
-
+    def getFingerCount(self, img, draw=True, thumb_con=-10):
+        
         # Get the finger positions and the img
         lmList,img = self.detector.findPosition(img, draw=True if draw == True else False)
 
@@ -20,17 +20,17 @@ class fingerCounter():
             # finger. its used to check if my hand is facing towards the cam or not
             if lmList[self.tipIds[0]][1] > lmList[self.tipIds[4]][1]:
 
-                # If the hand is not facing the camera it checks if the tip of the thumb's x coordinate - bone under 
+                # If the hand is not facing the camera it checks if the tip of the thumb's x coordinate - bone under
                 # the tip of the thumb's x coordinate is smaller than 15
-                if lmList[self.tipIds[0]-1][1] - lmList[self.tipIds[0]][1] < 15:
+                if lmList[self.tipIds[0]-1][1] - lmList[self.tipIds[0]][1] < thumb_con:
                     fingers.append(1)
                 else:
                     fingers.append(0)
             else:
 
-                # If the hand is facing the camera it checks if the tip of the thumb's x coordinate - bone under 
+                # If the hand is facing the camera it checks if the tip of the thumb's x coordinate - bone under
                 # the tip of the thumb's x coordinate is bigger than 15
-                if lmList[self.tipIds[0]-1][1] - lmList[self.tipIds[0]][1] > 15:
+                if lmList[self.tipIds[0]-1][1] - lmList[self.tipIds[0]][1] > thumb_con:
                     fingers.append(1)
                 else:
                     fingers.append(0)
